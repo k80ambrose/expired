bf3bd2468e2e48220170ffd5aa9ea407c8048d3d
k80 <81832771+k80ambrose@users.noreply.github.com>
2021-12-18 01:12:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
+var colour="random"; 
+var sparkles=70;
 
-
-var colour="random"; // "random" can be replaced with any valid colour ie: "red"...
-var sparkles=100;// increase of decrease for number of sparkles falling
-
-var x=ox=300;
+var x=ox=400;
 var y=oy=300;
 var swide=800;
 var shigh=600;
